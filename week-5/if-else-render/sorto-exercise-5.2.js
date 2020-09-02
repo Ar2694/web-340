53a5ced66a37094d3eeb896ad67d9ae9ca67b181
Arlix Sorto <arlix2694@gmail.com>
2020-09-02 17:55:34 -0500

    Exercise 5.2

---
+++ week-5/if-else-render/sorto-exercise-5.2.js
@@ -0,0 +1,31 @@
+/*
+============================================
+; Title: Exercise 5.2
+; Author: Professor Krasso
+; Date:   28 February 2019
+; Modified By Arlix Sorto
+; Modified Date: 9/2/2020
+; Description: EJS Templates
+;===========================================
+*/
+
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var app = express();
+
+app.set("views", path.resolve(__dirname,"views"));
+app.set("view engine", "ejs");
+
+var arrName = ["Jerry", "John", "Arlix", "Kenny", "Tom"];
+
+app.get("/", function(req,res){
+
+res.render("index",{
+  list_of_names: arrName,
+});
+});
+
+http.createServer(app).listen(8080, function(){
+  console.log("Application started on port 8080!");
+})
